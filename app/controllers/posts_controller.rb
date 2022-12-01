ca19689d1bc2ac534d74aa8adf11f68ec3b91dac
Jesus H <jshernandez3@uh.edu>
2022-12-01 21:10:56 +0000

    chatter app.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  include CableReady::Broadcaster
+
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
+  end
+  
+  def create
+    # @post = Post.new(post_params)
+    post = Post.create(post_params)
+    cable_ready["timeline"].insert_adjacent_html(
+      selector: "#timeline",
+      position: "afterbegin",
+      html: render_to_string(partial: "post", locals: {post: post})
+      )
+    cable_ready.broadcast
+    redirect_to posts_path
+    
+  end
+
+  private
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      # params.require(:post).permit(:username, :body, :likes_count, :reposts_count)
+      params.require(:post).permit(:body)
+    end
+end
