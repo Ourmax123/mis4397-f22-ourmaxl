90feb65d48d976fff110ceddc097492dfcbb7f11
Ourmax Lange <ourmaxlange1@gmail.com>
2022-12-11 19:10:25 +0000

    Added routes for explore and profile pages, profile static page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'explore/index'
+  get 'profile/index'
   resources :posts, only: [:index, :create]
+  resources :profile, only: [:index]
+  resources :explore, only: [:index]
   root "posts#index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
